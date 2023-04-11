7e2c4b7719bee08748f4c357dbf5aab5f0642ca5
LuisCepeda33 <LuisCepeda33>
2023-04-11 20:09:36 +0000

    test2

--- Test/Test1.py
+++ Test/Test1.py
@@ -1,7 +1,11 @@
 # Databricks notebook source
-print("hello world")
-
+print("hello world Jupyter")
 
 # COMMAND ----------
 
+for i in "schneider":
+  print(i)
 
+# COMMAND ----------
+
+print(1+1)
